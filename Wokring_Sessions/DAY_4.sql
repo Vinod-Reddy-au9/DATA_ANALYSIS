98a56a2d4be07c8644f8b46eeb14c43fac952fac
Vinod Reddy Kandhukuri <vinodreddy@Vinods-MBP.attlocal.net>
2022-08-15 13:17:32 -0500

    Project_1 Finished

---
+++ Wokring_Sessions/DAY_4.sql
@@ -0,0 +1,23 @@
+--DVDRENTAL
+
+--Display customerid’s whose rental duration is more than 5 days
+
+--Display customerid’s whose minimum rental duration is 1 day and
+--max rental duration is 8 days
+
+--Write a query to display actor lastname if the lastname is <=4,>4 &
+--<=6,>6 as short,medium and long
+
+--Display filmid’s having the title starts with Ali using string functions.
+
+
+--NORTH WIND
+--Display the max unitsinstock by supplierid,categoryid with all
+--combinations
+
+--Display the complete name of the employees whose hiredate is > 14
+
+--Display the lastname of the employees who is having same hiredate but
+--different hireyear
+
+--Display customerids whose having the contacttitle ends with ‘Manager’
